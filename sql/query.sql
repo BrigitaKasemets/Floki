ff2c6d2ac7210e39660e6c2f88052c32804bcb88
brigitakasemets <brigita.kasemets@vikk.ee>
2024-12-14 14:00:51 +0200

    query.sql

--- sql/query.sql
+++ sql/query.sql
@@ -1,7 +1,12 @@
-select count(*)
-from plant;
+SELECT count(*)
+FROM plant;
 
 SELECT *
 FROM plant
 WHERE name = 'cactus'
    or location = 'living room';
+
+SELECT plant.name, species_id
+    FROM plant
+    INNER JOIN species ON plant.species_id = species.id
+    WHERE species.name = 'cactus';
